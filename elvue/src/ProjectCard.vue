af89ee41f13ed38f2efc00cf94a85ed41424bc86
ovaphlow <jsfreecss@163.com>
2017-10-22 22:47:04 +0800

    .

---
+++ elvue/src/ProjectCard.vue
@@ -0,0 +1,59 @@
+<template>
+    <el-row>
+        <br/>
+        <el-col :span="6" v-for="(o, index) in 3" :key="o" :offset="index > 0 ? 3 : 0" style="padding-left: 20px; padding-right: 20px">
+            <el-card :body-style="{ padding: '0px' }">
+                <img src="http://element.eleme.io/static/hamburger.50e4091.png" class="image">
+                <div style="padding: 14px;">
+                    <span>好吃的汉堡</span>
+                    <div class="bottom clearfix">
+                        <time class="time">{{ currentDate }}</time>
+                        <el-button type="text" class="button">操作按钮</el-button>
+                    </div>
+                </div>
+            </el-card>
+        </el-col>
+    </el-row>
+</template>
+
+<style>
+  .time {
+    font-size: 13px;
+    color: #999;
+  }
+
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+  }
+
+  .button {
+    padding: 0;
+    float: right;
+  }
+
+  .image {
+    width: 100%;
+    display: block;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+      display: table;
+      content: "";
+  }
+
+  .clearfix:after {
+      clear: both
+  }
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      currentDate: new Date()
+    };
+  }
+}
+</script>
--- elvue/src/main.js
+++ elvue/src/main.js
@@ -1,11 +1,24 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
-import App from './App.vue'
+// import App from './App.vue'
+import Menu from './Menu.vue'
+import Carousel from './Carousel.vue'
+import ProjectCard from './ProjectCard.vue'
 
 Vue.use(ElementUI)
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#menu',
+    render: h => h(Menu)
+})
+
+new Vue({
+    el: '#carousel',
+    render: h => h(Carousel)
+})
+
+new Vue({
+    el: '#project-card',
+    render: h => h(ProjectCard)
 })
